8b1c144aafa23125e8100e05c7f5fbcddad01d25
alextozetti <alextcc7187@gmail.com>
2024-06-11 05:34:53 -0300

    teste de push para o docker hub

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# Rodar as imagens criadas localmente
 docker-compose down
 docker build -t backend-prod:latest ./backend
 docker build -t frontend-prod:latest ./frontend
 docker-compose up --build --force-recreate --remove-orphans
+
+# Baixar as imagens do docker hub e rodar
+# docker-compose down
+# docker-compose pull
+# docker-compose up --build --force-recreate --remove-orphans
