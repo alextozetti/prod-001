2089a0b2d13c31f4419e4090c633a51205caa23a
alextozetti <alextcc7187@gmail.com>
2024-06-10 21:55:49 -0300

    frontend

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 docker-compose down
 docker build -t backend-prod:latest ./backend
-#docker build -t frontend-prod:latest ./frontend
+docker build -t frontend-prod:latest ./frontend
 docker-compose up --build --force-recreate --remove-orphans
